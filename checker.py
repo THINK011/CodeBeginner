0a96b78c89436344fa4d185b9eceee1712a5c5c9
Think...... <mgmoon733@gmail.com>
2025-10-23 21:41:30 +0700

    Add basic structure for password strength checker
    Added initial structure for a password strength checker.

--- checker.py
+++ checker.py
@@ -1,1 +1,14 @@
+BasicCode
+
+#Password Strength Checker
+
+import string
+
+#Ask user input password = input("Enter your password:> ")
+
+#Use Sum total = sum([length,lower,upper,digit,special])
+
+Evaluate Strength
+
+Check not conditions
 
