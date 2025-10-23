b4ef9dc0e2ef4ec00ceaeeb6786866b06e40017d
Think...... <mgmoon733@gmail.com>
2025-10-23 21:50:46 +0700

    TD LI

--- step1.py
+++ step1.py
@@ -1,1 +1,27 @@
+# To Do List
 
+tasks = []
+while True:
+    print("\n--- ToDoList ---")
+    print("1. Add Task")
+    print("2. Show Task")
+    print("3. Exit")
+
+    choice = input("Choose one thing: ")
+
+    if choice == "1":
+        task = input("Enter a new task: ")
+        tasks.append(task)
+        print(f"Tasked Added {task}")
+    elif choice == "2":
+        print("\nYour Tasks")
+        if not tasks:
+            print("No tasks yet")
+        else:
+            for i, t in enumerate(tasks, start=1):
+                print(f"{i}. {t}...")
+    elif choice == "3":
+        print("Exiting Goodbye")
+        break
+    else:
+        print("Invalid choice")
