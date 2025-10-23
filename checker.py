39036e676e84511f9b8f7b1c936a52e140b370c1
Think...... <mgmoon733@gmail.com>
2025-10-23 21:44:00 +0700

    cy

--- checker.py
+++ checker.py
@@ -1,14 +1,36 @@
-BasicCode
-
 #Password Strength Checker
 
 import string
 
-#Ask user input password = input("Enter your password:> ")
+#Ask user input 
+password = input("Enter your password:> ")
 
-#Use Sum total = sum([length,lower,upper,digit,special])
+#Check Conditions
+length = len(password) >= 8  # they're output boolen
+lower = any(char.islower() for char in password) # any() output boolen but need to True at least one #.islower() decided True or False
+upper = any(char.isupper() for char in password) 
+digit = any(char.isdigit() for char in password)
+special = any(char in string.punctuation for char in password)
 
-Evaluate Strength
+#Use Sum
+total = sum([length,lower,upper,digit,special])
 
-Check not conditions
+# Evaluate Strength
+if total <= 2:
+    print("Weak")
+elif total == 3 or total == 4:
+    print("Medium")
+else:
+    print("Strong")
 
+# Check not conditions
+print("\nPassword Checker")
+if not length:
+    print("- Must to be length 8 character")
+if not lower:
+    print("- Must be lowercase letters")
+    print("- Must be uppercase letters")
+if not digit:
+    print("- Must be include digit")
+if not special:
+    print("-- Must be include strong special")
